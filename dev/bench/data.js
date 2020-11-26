66133dc969580c82d01ad2881aa71a9d4744c121
github-action-benchmark <github@users.noreply.github.com>
2020-11-26 03:34:34 +0000

    add Benchmark (go) benchmark result for 43f77647dc37e791a6fd0a0304e006dd74ff2b6c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1606361110417,
+  "lastUpdate": 1606361674156,
   "repoUrl": "https://github.com/jimeh/undent",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 22.3,
             "unit": "ns/op",
             "extra": "57147370 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "committer": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "distinct": true,
+          "id": "43f77647dc37e791a6fd0a0304e006dd74ff2b6c",
+          "message": "ci(github): add CI workflow for GitHub Actions",
+          "timestamp": "2020-11-26T03:33:30Z",
+          "tree_id": "9de2eb87663a2a6f687bb8f10687a9dcf7455b31",
+          "url": "https://github.com/jimeh/undent/commit/43f77647dc37e791a6fd0a0304e006dd74ff2b6c"
+        },
+        "date": 1606361671097,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBytes_empty",
+            "value": 23.2,
+            "unit": "ns/op",
+            "extra": "45811638 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkString_empty",
+            "value": 21.5,
+            "unit": "ns/op",
+            "extra": "55764522 times\n2 procs"
           }
         ]
       }
