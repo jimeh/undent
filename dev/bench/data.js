c15253322053aee4d1c15630ebdddbdca6763cef
github-action-benchmark <github@users.noreply.github.com>
2020-11-26 03:25:10 +0000

    add Benchmark (go) benchmark result for 2819ce650a30ee95f360311ec9de8095c13cadba

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1606361110417,
+  "repoUrl": "https://github.com/jimeh/undent",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "committer": {
+            "email": "contact@jimeh.me",
+            "name": "Jim Myhrberg",
+            "username": "jimeh"
+          },
+          "distinct": true,
+          "id": "2819ce650a30ee95f360311ec9de8095c13cadba",
+          "message": "ci(github): add CI workflow for GitHub Actions",
+          "timestamp": "2020-11-26T03:24:12Z",
+          "tree_id": "f3b485bbaf715b4db53e6a324428123043256590",
+          "url": "https://github.com/jimeh/undent/commit/2819ce650a30ee95f360311ec9de8095c13cadba"
+        },
+        "date": 1606361109880,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBytes__empty",
+            "value": 22.6,
+            "unit": "ns/op",
+            "extra": "51463430 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkString__empty",
+            "value": 22.3,
+            "unit": "ns/op",
+            "extra": "57147370 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
